a0dc4a38c21d2984430786cd907342b5486fb045
sarucm1017 <98sarucm@gmail.com>
2024-08-10 18:22:11 +0530

    doctor form controller

--- controller/DoctorsFormController.js
+++ controller/DoctorsFormController.js
@@ -60,7 +60,9 @@ const newDoctor = asyncHandler(async (req, res) => {
       userId: user._id ,
       userName: user.name
     });
-
+  
+    console.log({Doctor, userId :Doctor.userId});
+    
     res.status(201).json({Doctor, userId :Doctor.userId});
   } catch (error) {
     res.status(400).json({ message: error.message });

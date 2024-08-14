8cce71a28fb7b2458f29aa41047e47e797f2750d
sarucm1017 <98sarucm@gmail.com>
2024-08-14 10:34:40 +0530

    doctor route

--- routes/doctorsFormRoutes.js
+++ routes/doctorsFormRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();    
-const {newDoctor, getDoctorById, getDoctorDetailsById} = require("../controller/DoctorsFormController");
+const {newDoctor, getDoctorById, getDoctorDetailsById, updateDoctor} = require("../controller/DoctorsFormController");
 const { getAllDoctors } = require("../controller/Doctors/DoctorsController");
 
 
@@ -8,6 +8,7 @@ router.route("/forms").post(newDoctor);
 router.route("/forms").get(getAllDoctors);
 router.route("/:userId").get(getDoctorById);
 router.route("/:userId").get(getDoctorDetailsById);
+router.route("/:userId").put(updateDoctor);
 
 
 

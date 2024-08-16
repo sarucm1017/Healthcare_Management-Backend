6ab47bd9a0ae27af98d74806b6d5310bb6a3cb27
sarucm1017 <98sarucm@gmail.com>
2024-08-16 12:28:21 +0530

    report Model

---
+++ models/reportModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const reportSchema = new mongoose.Schema({
+    patientName: { type: String, required: true },
+  bloodGroup: { type: String, required: true },
+  pulseRate: { type: String, required: true },
+  breathingRate: { type: String, required: true },
+  bodyTemperature: { type: String, required: true },
+  bloodPressure: { type: String, required: true },
+  oxygenSaturation: { type: String, required: true },
+  height: { type: String, required: true },
+  weight: { type: String, required: true },
+  diagnosis: { type: String, required: true },
+  symptoms: { type: String, required: true },
+  currentMedications: { type: String, required: true },
+  prescribedTreatment: { type: String, required: true },
+})
